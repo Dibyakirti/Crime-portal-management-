68de12f276bfc19d8a4012ebb28c5847b57f81ee
Dibyakirti <23057021@kiit.ac.in>
2025-05-27 16:56:13 +0530

    Add files via upload

---
+++ crime/crime_portal.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 08, 2021 at 01:59 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crime_portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `c_id` int(11) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `type_crime` varchar(50) NOT NULL,
+  `d_o_c` date NOT NULL,
+  `description` varchar(7000) NOT NULL,
+  `inc_status` varchar(50) DEFAULT 'Unassigned',
+  `pol_status` varchar(50) DEFAULT 'null',
+  `p_id` varchar(50) DEFAULT 'Null'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaint`
+--
+
+INSERT INTO `complaint` (`c_id`, `a_no`, `location`, `type_crime`, `d_o_c`, `description`, `inc_status`, `pol_status`, `p_id`) VALUES
+(2, 123456789012, 'Mancheswar', 'Theft', '2021-04-08', 'Testing documents missing', 'Assigned', 'ChargeSheet Filed', '2102'),
+(3, 123456789012, 'Saheednagar', 'Murder', '2021-04-08', 'dog', 'Assigned', 'ChargeSheet Filed', '2001');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `head`
+--
+
+CREATE TABLE `head` (
+  `h_id` varchar(50) NOT NULL,
+  `h_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `head`
+--
+
+INSERT INTO `head` (`h_id`, `h_pass`) VALUES
+('head@kp', 'head');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police`
+--
+
+CREATE TABLE `police` (
+  `p_name` varchar(50) NOT NULL,
+  `p_id` varchar(50) NOT NULL,
+  `spec` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `p_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `police`
+--
+
+INSERT INTO `police` (`p_name`, `p_id`, `spec`, `location`, `p_pass`) VALUES
+('Girija Prasad', '2001', 'Shooter', 'Saheednagar', '123'),
+('Tejesvi Singh', '2101', 'IIC', 'Mancheswar', '123'),
+('Tarasankar', '2102', 'Inspector', 'Mancheswar', '123'),
+('Manish Singh', 'a101', 'Murder', 'Anandapur', 'manish'),
+('Jay Singh', 'a102', 'All', 'Anandapur', 'jay'),
+('Suvendu Ghosh', 't101', 'Robbery', 'Tollygunge', 'suvendu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police_station`
+--
+
+CREATE TABLE `police_station` (
+  `i_id` varchar(50) NOT NULL,
+  `i_name` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `i_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `police_station`
+--
+
+INSERT INTO `police_station` (`i_id`, `i_name`, `location`, `i_pass`) VALUES
+('1258', 'Tara Sankar Panda', 'Mancheswar', '123'),
+('1259', 'Satyakam Jena', 'Saheednagar', '123'),
+('2000', 'Manasiz', 'khandagiri', 'mana');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `update_case`
+--
+
+CREATE TABLE `update_case` (
+  `c_id` int(11) NOT NULL,
+  `d_o_u` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `case_update` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `update_case`
+--
+
+INSERT INTO `update_case` (`c_id`, `d_o_u`, `case_update`) VALUES
+(2, '2021-04-08 05:35:03', 'Criminal Verified'),
+(2, '2021-04-08 05:35:25', 'Criminal Caught'),
+(2, '2021-04-08 05:35:31', 'Criminal Interrogated'),
+(2, '2021-04-08 05:35:38', 'Criminal Accepted the Crime'),
+(2, '2021-04-08 05:35:44', 'Criminal Charged'),
+(3, '2021-04-08 08:19:42', 'Criminal Verified');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `u_name` varchar(50) NOT NULL,
+  `u_id` varchar(50) NOT NULL,
+  `u_pass` varchar(50) NOT NULL,
+  `u_addr` varchar(100) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `gen` varchar(15) NOT NULL,
+  `mob` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`u_name`, `u_id`, `u_pass`, `u_addr`, `a_no`, `gen`, `mob`) VALUES
+('Satyansh Kumar', 'satyansh123@gmail.com', 'satyansh', 'Ranchi', 123214521452, 'Male', 9854123654),
+('Debasis Mishra', 'debasis.sysora@gmail.com', '123456', 'D-7, GGP', 123456789012, 'Male', 9938632105);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `police`
+--
+ALTER TABLE `police`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `police_station`
+--
+ALTER TABLE `police_station`
+  ADD PRIMARY KEY (`i_id`),
+  ADD UNIQUE KEY `location` (`location`);
+
+--
+-- Indexes for table `update_case`
+--
+ALTER TABLE `update_case`
+  ADD UNIQUE KEY `d_o_u` (`d_o_u`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`a_no`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `mob` (`mob`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
